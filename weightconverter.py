42cc7bcddaac89059c05dc5f38217a658665a0cc
codelytical <75156634+codelytical@users.noreply.github.com>
2020-11-28 14:46:35 +0800

    Add files via upload
    Program Objectives:
    
    Program will ask user to input the weight either in kg or lbs
    Program will ask user the unit for the weight entered
    Program will ask user to choose which unit to convert
    Program will print an output message

---
+++ weightconverter.py
@@ -0,0 +1,25 @@
+#author codelytical
+pounds = 2.20462
+weightinput = float(input("Please enter your weight: "))
+unit = input("What is the unit of the weight you input? (kgs or lbs) ")
+choice = int(input("Enter 1 for kgs and 2 for lbs to convert your weight: "))
+
+if unit == "kgs" and choice == 2:
+    convertweight = weightinput * pounds
+    print("Your weight in pounds is {:.2f} lbs".format(convertweight))
+elif unit == "lbs" and choice == 1:
+    convertweight =  weightinput / pounds
+    print("Your weight in kilograms is {:.2f} kg".format(convertweight))
+else:
+    print("You have entered the same weight units!".format(weightinput))
+
+
+    
+    
+    
+    
+
+
+
+
+            
